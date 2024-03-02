fa6da4ec9b986635ddeb8a5519d8162e786120b8
XebazX <sebastianhellsing1999@gmail.com>
2024-03-02 16:06:33 -0500

    sepa up

--- members/views.py
+++ members/views.py
@@ -10,8 +10,8 @@ def members(request):
   }
   return HttpResponse(template.render(context, request))
   
-def details(request, id):
-  mymember = Member.objects.get(id=id)
+def details(request, slug):
+  mymember = Member.objects.get(slug=slug)
   template = loader.get_template('details.html')
   context = {
     'mymember': mymember,
@@ -23,9 +23,8 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Manzana','Piña','Banano','Naranja','Mandarina','Fresa','Maracuya','Lulo',],
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request))  
